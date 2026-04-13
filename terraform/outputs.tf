135421ba561f65b382a745db07c8c67197ac9e41
Jasmit Tarang <jaytarang92@gmail.com>
2026-04-13 07:07:39 -0500

    fix(infra): remove stale DNS output reference

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,10 +2,6 @@ output "ec2_public_ip" {
   value = aws_eip.web_eip.public_ip
 }
 
-output "route53_name_servers" {
-  description = "Name servers for the new Route53 Zone"
-  value       = aws_route53_zone.main.name_servers
-}
 
 output "aws_secretsmanager_secret_arn" {
   value = aws_secretsmanager_secret.boostlog_secrets.arn
